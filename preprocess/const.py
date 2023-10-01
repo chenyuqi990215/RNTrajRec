86b31f46f10237837c2beb9c25bb9b0b6d7be506
Yuqi Chen <42923882+chenyuqi990215@users.noreply.github.com>
2023-10-01 12:43:45 +0800

    Create const.py

---
+++ preprocess/const.py
@@ -0,0 +1,3 @@
+candi_highway_types = {'motorway': 7, 'trunk': 6, 'primary': 5, 'secondary': 4, 'tertiary': 3,
+                       'unclassified': 2, 'residential': 1, 'motorway_link': 7, 'trunk_link': 6, 'primary_link': 5,
+                       'secondary_link': 4, 'tertiary_link': 3, 'living_street': 2, 'service': 0, 'road': 2}
